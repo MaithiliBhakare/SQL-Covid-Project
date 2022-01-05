51f45f60b22ed60791a4d2e19ffd811e74e612ce
Maithili Bhakare <bhakaremaithili@gmail.com>
2022-01-05 08:39:25 +0530

    Update Covid Project1.sql

--- Covid Project1.sql
+++ Covid Project1.sql
@@ -7,16 +7,22 @@ SELECT location, date, total_cases, new_cases, total_deaths, population
  where continent is not null
 order by 1,2
 
+-- Total Cases vs Total Deaths
+
 SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
  FROM [Portfolio project1]..[covid deaths]
  where location like '%India%'
 order by 1,2
+
+-- Total Cases vs Population
 
 SELECT location, date, population, total_cases, (total_cases/population)*100 as Casespermillion
  FROM [Portfolio project1]..[covid deaths]
  where continent is not null
  --where location like '%India%'
 order by 1,2
+
+-- Countries with Highest Infection Rate compared to Population
 
 SELECT location, population, max(total_cases) as HighestInfection, max((total_cases/population)*100) as PercentCasespermillion
  FROM [Portfolio project1]..[covid deaths]
@@ -25,17 +31,23 @@ SELECT location, population, max(total_cases) as HighestInfection, max((total_ca
  Group by location, population
 order by PercentCasespermillion desc
 
+-- Countries with Highest Death Count per Population
+
 SELECT location, max(cast(total_deaths as int)) as HighestDeath
  FROM [Portfolio project1]..[covid deaths]
  where continent is not null
  Group by location
 order by HighestDeath desc
 
+-- Showing contintents with the highest death count per population
+
 SELECT continent, max(cast(total_deaths as int)) as HighestDeath
  FROM [Portfolio project1]..[covid deaths]
  where continent is not null
  Group by continent
 order by HighestDeath desc
+
+-- GLOBAL NUMBERS
 
 SELECT date, SUM(cast(new_cases as int)) as total_cases, SUM(CAST(new_deaths as int)) as total_deaths, ((SUM(CAST(new_deaths as int))/SUM(new_cases))*100) as DeathPercentage --total_deaths, total_cases, (total_deaths/total_cases)*100 as DeathPercent
  FROM [Portfolio project1]..[covid deaths]
@@ -44,6 +56,8 @@ SELECT date, SUM(cast(new_cases as int)) as total_cases, SUM(CAST(new_deaths as 
  Group by date
 order by DeathPercentage desc
 
+--Total Population vs Vaccinations
+
 SELECT SUM(cast(new_cases as int)) as total_cases, SUM(CAST(new_deaths as int)) as total_deaths, ((SUM(CAST(new_deaths as int))/SUM(new_cases))*100) as DeathPercentage --total_deaths, total_cases, (total_deaths/total_cases)*100 as DeathPercent
  FROM [Portfolio project1]..[covid deaths]
  where continent is not null
@@ -51,6 +65,7 @@ SELECT SUM(cast(new_cases as int)) as total_cases, SUM(CAST(new_deaths as int)) 
  --Group by date
 order by DeathPercentage desc
 
+-- Using CTE to perform Calculation on Partition By in previous query
 
 SELECT x.continent, x.location, x.date, x.population, y.new_vaccinations, SUM(convert(bigint,y.new_vaccinations)) OVER (Partition by x.location order by x.date, x.location) as RollingPeopleVac
 FROM [Portfolio project1]..[covid deaths] x
@@ -75,6 +90,8 @@ where x.continent is not null
 SELECT *, (RollingPeopleVac/population)*100
 FROM PopvsVac
 
+-- Using Temp Table to perform Calculation on Partition By in previous query
+
 Drop Table if exists PercentPopulVacc
 Create Table PercentPopulVacc
 (
@@ -97,6 +114,7 @@ where x.continent is not null
 SELECT *, (RollingPeopleVac/population)*100 as RollingPeopleVaccpermil
 FROM PercentPopulVacc
 
+-- Creating View to store data for later visualizations
 
 CREATE VIEW PercentPopulVacci AS
 SELECT x.continent, x.location, x.date, x.population, y.new_vaccinations, SUM(convert(bigint,y.new_vaccinations)) OVER (Partition by x.location order by x.date, x.location) as RollingPeopleVac
